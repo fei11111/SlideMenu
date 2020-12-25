541cb07f8474bd6b0edb2b8c638104b495a4db87
fei11111 <455842151@qq.com>
2020-12-25 17:00:05 +0800

    init

---
+++ app/src/main/java/com/fei/slidemenu/SlideMenu.java
@@ -0,0 +1,31 @@
+package com.fei.slidemenu;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+/**
+ * @ClassName: SlideMenu
+ * @Description: 描述
+ * @Author: Fei
+ * @CreateDate: 2020/12/25 15:26
+ * @UpdateUser: Fei
+ * @UpdateDate: 2020/12/25 15:26
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+
+public class SlideMenu extends HorizontalScrollView {
+
+    public SlideMenu(Context context) {
+        super(context);
+    }
+
+    public SlideMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SlideMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
